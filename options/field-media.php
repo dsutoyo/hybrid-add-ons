06039f98767a2056c89d9082870ea7b599a10a00
David Sutoyo <sutoyod@yahoo.com>
2011-06-12 14:30:51 -0700

    Fix media remove image checkbox style

--- options/field-media.php
+++ options/field-media.php
@@ -6,9 +6,9 @@ function remix_options_media($var) {
     if ( $the_theme_options[$var] ) {
       echo '<img src="' . $the_theme_options[$var] . '" />';
       echo '<p><small><em>Preview may be smaller than actual image size</em></small></p>';
-      echo '<div class="remove_image">';
-      remix_options_checkbox( 'delete_' . $var );
-      remix_options_label_for( 'delete_' . $var, 'Remove image', 'option_checkbox_label' );
+      echo '<div class="remove_image clearfix">';
+      remix_options_checkbox( 'delete_' . $var, 'Remove image' );
+      //remix_options_label_for( 'delete_' . $var, 'Remove image', 'option_checkbox_label' );
       echo '</div>';
     }
     echo '<div class="element">';
